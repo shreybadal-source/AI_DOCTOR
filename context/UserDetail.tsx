33ccf5aac14416807698e8ff9e35967a4cfab266
shreybadal-source <shreybadal@gmail.com>
2025-12-08 12:31:56 +0530

    Fix UserDetail import path

--- context/UserDetail.tsx
+++ context/UserDetail.tsx
@@ -1,4 +1,4 @@
 import { createContext } from "react";
-import { UseDetail } from "@/app/[locale]/provider";
+import { UseDetail } from "@/app/provider";
 
 export const UserDetailContext = createContext<UseDetail | undefined>(undefined);
